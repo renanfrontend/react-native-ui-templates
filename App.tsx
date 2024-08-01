8bb5aa23fe6baef98089940a70f9163384a55945
Renan Augusto dos Santos <146970438+renanfrontend@users.noreply.github.com>
2024-08-01 11:23:13 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import AppControlFlow from './src/AppControlFlow';
+
+const App = () => {
+  return <AppControlFlow />;
+};
+
+export default App;
